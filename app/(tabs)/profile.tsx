2923a14b102b9dbe7954262675bf925612d25443
Christopher Obocha <christopherobocha@gmail.com>
2024-12-21 17:51:16 +0000

    image will pick but is referencing local storage

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,30 +1,68 @@
+import { Image } from 'expo-image';
+import * as ImagePicker from 'expo-image-picker';
 import { Stack } from 'expo-router';
-import { StyleSheet, View, Text } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-import { ScreenContent } from '~/components/ScreenContent';
-import { useAuthStore } from '~/stores/useAuthStore';
+import ScreenContainer from '~/components/ScreenContainer';
+import Spacer from '~/components/Spacer';
 import { useProfileStore } from '~/stores/useProfileStore';
 
 export default function Profile() {
-  const { user } = useAuthStore();
-  const { profile } = useProfileStore();
-  // console.log('user: ', user);
-  console.log('profile: ', profile);
+  const { profile, updateProfile } = useProfileStore();
+  const [uploading, setUploading] = useState(false);
+
+  const pickImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: 'images',
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setUploading(true);
+        const imageUri = result.assets[0].uri;
+
+        //Update the profile with the new avatar URL
+        await updateProfile({
+          ...profile,
+          avatar_url: imageUri, // Using the local image URI for now
+        });
+      }
+    } catch (error) {
+      console.error('Error uploading image:', error);
+      // Handle error appropriately
+    } finally {
+      setUploading(false);
+    }
+  };
 
   return (
     <>
       <Stack.Screen options={{ title: 'Profile' }} />
-      <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/profile.tsx" title="Profile" />
-        <Text>{profile?.username}</Text>
-      </View>
+      <ScreenContainer>
+        <>
+          <TouchableOpacity onPress={pickImage}>
+            <Image
+              source={profile?.avatar_url || 'https://picsum.photos/200/300'}
+              style={styles.image}
+            />
+          </TouchableOpacity>
+          <Spacer height={20} />
+          <Text className="text-2xl font-bold">{profile?.username}</Text>
+        </>
+      </ScreenContainer>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
+  container: {},
+  image: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
   },
 });
