334ee9915f3c254be51d5aa023ebc86822883179
Christopher Obocha <christopherobocha@gmail.com>
2024-12-21 13:59:59 +0000

    fetch profile data

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Button } from 'react-native';
 
 import AuthTest from '../../components/AuthTest';
 import DatabaseTest from '../../components/DatabaseTest';
 // import useAuthStore from '../../stores/useAuthStore';
 import { useAuthStore } from '../../stores/useAuthStore';
+import { useProfileStore } from '../../stores/useProfileStore';
 
 export default function TabOneScreen() {
   // const signOut = useAuthStore((state) => state.signOut);
-  const { signOut } = useAuthStore();
+  const { user, signOut } = useAuthStore();
+  const { fetchProfile } = useProfileStore();
+
+  useEffect(() => {
+    if (user) {
+      fetchProfile(user.id);
+    }
+  }, [user]);
 
   return (
     <View className="flex-1 p-4">
